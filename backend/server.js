7460427443ddc2ecddf5699442e419ce99d8eab7
Anthony <bondolo90@gmail.com>
2024-03-30 21:32:11 +0300

    Connecting to MongoDB and setting up users schema

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import mongoose from 'mongoose';
 import expensesRoutes from './routes/expensesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 
@@ -11,6 +12,15 @@ const app = express();
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
+
+// Connect to MongoDB
+const mongoURI = 'mongodb+srv://bondolo90:RoTKuRHDx2KZZHth@cluster0.qpajh96.mongodb.net/';
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Error connecting to MongoDB:', err));
 
 // Routes
 app.use('/expenses', expensesRoutes);
